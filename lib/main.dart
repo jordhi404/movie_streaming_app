3b13b5632190d76677bd33998019d6c3d6f6a87f
jordhi404 <79629470+jordhi404@users.noreply.github.com>
2023-02-20 15:52:08 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:movie_app/providers/account.dart';
+import 'package:movie_app/providers/entry.dart';
+import 'package:movie_app/providers/watchlist.dart';
+import 'package:movie_app/screens/navigation.dart';
+import 'package:movie_app/screens/onboarding.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AccountProvider()),
+        ChangeNotifierProvider(create: (context) => EntryProvider()),
+        ChangeNotifierProvider(create: (context) => WatchListProvider()),
+      ],
+      child: const Main(),
+    )
+  );
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appflix',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FutureBuilder(
+        future: context.read<AccountProvider>().isValid(),
+        builder: (context, snapshot) => context.watch<AccountProvider>().session == null ? const OnboardingScreen() : const NavScreen(),
+      )
+    );
+  }
+}
